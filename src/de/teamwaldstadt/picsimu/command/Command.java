500fd9ded905ff99edee755263ab9e378e0befe7
Innoberger <fynn.arnold@gmail.com>
2019-04-01 15:46:14 +0200

    filled Command super class

--- src/de/teamwaldstadt/picsimu/command/Command.java
+++ src/de/teamwaldstadt/picsimu/command/Command.java
@@ -1,9 +1,32 @@
 package de.teamwaldstadt.picsimu.command;
 
-public class Command {
+public abstract class Command {
 	
 	/*
-	 * This is the basic command super class.
+	 * Command super class
 	 */
+	
+	private Command next;
+	
+	public Command() {}
+	
+	public abstract void execute();
+	
+	public void next() {
+		if (this.next == null) {
+			System.out.println("Done!");
+			return;
+		}
+		
+		this.next.execute();
+	}
+	
+	public Command getNext() {
+		return this.next;
+	}
+	
+	public void setNext(Command next) {
+		this.next = next;
+	}
 
 }
