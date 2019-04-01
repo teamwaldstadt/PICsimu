929539632d83236fee689961f7b4fea623a31d2e
Innoberger <fynn.arnold@gmail.com>
2019-04-01 15:11:38 +0200

    initial upload

---
+++ src/de/teamwaldstadt/picsimu/Main.java
@@ -0,0 +1,12 @@
+package de.teamwaldstadt.picsimu;
+
+public class Main {
+	
+	public static final String PGM_NAME = "PICsimu";
+	public static final String PGM_VERSION = "0.0.1";
+
+	public static void main(String[] args) {
+		System.out.println("Hello World, this is " + PGM_NAME + " " + PGM_VERSION);
+	}
+
+}
---
+++ src/de/teamwaldstadt/picsimu/command/Command.java
@@ -0,0 +1,9 @@
+package de.teamwaldstadt.picsimu.command;
+
+public class Command {
+	
+	/*
+	 * This is the basic command super class.
+	 */
+
+}
