b8eed6aac54ba8e385869efc20f6620bf145ce24
Innoberger <fynn.arnold@gmail.com>
2019-04-09 09:32:56 +0200

    completed command movwf

--- src/de/teamwaldstadt/picsimu/command/CommandMOVWF.java
+++ src/de/teamwaldstadt/picsimu/command/CommandMOVWF.java
@@ -1,11 +1,33 @@
 package de.teamwaldstadt.picsimu.command;
 
+import de.teamwaldstadt.picsimu.Main;
+import de.teamwaldstadt.picsimu.storage.GeneralRegister;
+import de.teamwaldstadt.picsimu.storage.SpecialRegister;
+
 public class CommandMOVWF extends CommandExecutor {
+	
+	public CommandMOVWF(int arguments) throws Exception {
+		super.setArguments(arguments);
+	}
 
 	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
+	public void execute() throws Exception {
+		int w = Main.STORAGE.getW();
 		
+		try {
+			GeneralRegister register = new GeneralRegister(super.getArguments());
+			
+			Main.STORAGE.setRegister(register, w);
+			super.affectStatus(Command.MOVWF, 0);
+		} catch (Exception e) {
+			SpecialRegister register = SpecialRegister.atAddress(super.getArguments());
+			
+			Main.STORAGE.setRegister(register, w);
+			super.affectStatus(Command.MOVWF, 0);
+		}
+
+		super.affectStatus(Command.MOVWF, 0);
+		super.incrementPC();
 	}
 
 }
