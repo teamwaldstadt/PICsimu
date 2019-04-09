9ae1fa5f169b12fb41068e067ac1ae1f0ff792ad
Innoberger <fynn.arnold@gmail.com>
2019-04-09 20:58:33 +0200

    bugfix: general register not working on bank 1

--- src/de/teamwaldstadt/picsimu/storage/GeneralRegister.java
+++ src/de/teamwaldstadt/picsimu/storage/GeneralRegister.java
@@ -8,12 +8,14 @@ public class GeneralRegister {
 	private int address;
 
 	public GeneralRegister(int address) throws Exception {
-		if (address < FIRST_ADDRESS || address > LAST_ADDRESS) {
+		if (address >= FIRST_ADDRESS && address <= LAST_ADDRESS) {
+			this.address = address;
+		} else if ((address >= (FIRST_ADDRESS + Bank.BEGIN_OF_BANK_1)) && (address <= (LAST_ADDRESS + Bank.BEGIN_OF_BANK_1))) {
+			this.address = address - Bank.BEGIN_OF_BANK_1;
+		} else {
 			throw new Exception("Not a general purpose register (out of range: " + String.format("%2X", FIRST_ADDRESS)
-					+ " to " + String.format("%2X", LAST_ADDRESS) + "): " + String.format("%2X", address));
+			+ " to " + String.format("%2X", LAST_ADDRESS) + "): " + String.format("%2X", address));
 		}
-
-		this.address = address;
 	}
 
 	public int getAddress() {
