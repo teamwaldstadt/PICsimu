9ea8337299374fd402a0c7dc75b40d36de09fc3d
Innoberger <fynn.arnold@gmail.com>
2019-04-11 20:58:05 +0200

    completed commands incf, decf, movf and swapf

--- src/de/teamwaldstadt/picsimu/command/CommandDECF.java
+++ src/de/teamwaldstadt/picsimu/command/CommandDECF.java
@@ -1,11 +1,49 @@
 package de.teamwaldstadt.picsimu.command;
 
+import de.teamwaldstadt.picsimu.Main;
+import de.teamwaldstadt.picsimu.storage.Storage;
+
 public class CommandDECF extends CommandExecutor {
+	
+	private boolean isDestinationBitSet;
+	private int fileRegister;
+
+	public CommandDECF(int arguments) throws Exception {
+		super.setArguments(arguments);
+		
+		this.isDestinationBitSet = Storage.extractBitsFromIntNumber(arguments, 0, 1, 8) == 1;
+		this.fileRegister = Storage.extractBitsFromIntNumber(arguments, 1, 8, 8);
+	}
 
 	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
+	public void execute() throws Exception {
+		int f = Main.STORAGE.getRegister(this.fileRegister, false);
+		int result = f - 1;
 		
+		// TODO wie verhält sich der Command, wenn f bereits 0 ist?
+		if (result == -1) {
+			result = 0;
+		}
+		
+		super.affectStatus(Command.DECF, result);
+		
+		result &= 0xFF; // result maskieren
+		
+		if (this.isDestinationBitSet) {
+			Main.STORAGE.setRegister(this.fileRegister, result, false);
+		} else {
+			Main.STORAGE.setW(result);
+		}
+		
+		super.incrementPC();
+	}
+	
+	public boolean isDestinationBitSet() {
+		return this.isDestinationBitSet;
+	}
+	
+	public int getFileRegister() {
+		return this.fileRegister;
 	}
 
 }
--- src/de/teamwaldstadt/picsimu/command/CommandINCF.java
+++ src/de/teamwaldstadt/picsimu/command/CommandINCF.java
@@ -1,11 +1,44 @@
 package de.teamwaldstadt.picsimu.command;
 
+import de.teamwaldstadt.picsimu.Main;
+import de.teamwaldstadt.picsimu.storage.Storage;
+
 public class CommandINCF extends CommandExecutor {
+	
+	private boolean isDestinationBitSet;
+	private int fileRegister;
+
+	public CommandINCF(int arguments) throws Exception {
+		super.setArguments(arguments);
+		
+		this.isDestinationBitSet = Storage.extractBitsFromIntNumber(arguments, 0, 1, 8) == 1;
+		this.fileRegister = Storage.extractBitsFromIntNumber(arguments, 1, 8, 8);
+	}
 
 	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
+	public void execute() throws Exception {
+		int f = Main.STORAGE.getRegister(this.fileRegister, false);
+		int result = f + 1;
 		
+		super.affectStatus(Command.INCF, result);
+		
+		result &= 0xFF; // result maskieren
+		
+		if (this.isDestinationBitSet) {
+			Main.STORAGE.setRegister(this.fileRegister, result, false);
+		} else {
+			Main.STORAGE.setW(result);
+		}
+		
+		super.incrementPC();
+	}
+	
+	public boolean isDestinationBitSet() {
+		return this.isDestinationBitSet;
+	}
+	
+	public int getFileRegister() {
+		return this.fileRegister;
 	}
 
 }
